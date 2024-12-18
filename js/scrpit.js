8be324fc8482b29fd5d2906be6393f28ef971576
Alailson-Nascimento <alailsonnascimento707@gmail.com>
2024-12-18 16:43:22 -0300

    :tada: initial commit

---
+++ js/scrpit.js
@@ -0,0 +1,13 @@
+function toggleModel() {
+    const html = document.documentElement;
+    html.classList.toggle("light");
+
+    const img = document.querySelector("#profile img ");
+
+    if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Alailson-photo-black.png");
+    } else {
+    ("assets/Alailson-photo-blue.png");
+    img.setAttribute("src", "assets/Alailson-photo-blue.png");
+    }
+}
